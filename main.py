7974994470c90de575384d09cac2a37b38371c6c
Muhammed Sharvan C <sharvancmampad@gmail.com>
2024-09-06 22:24:22 +0530

    added function to capture answers

--- main.py
+++ main.py
@@ -10,31 +10,55 @@ st.markdown('<br>', unsafe_allow_html=True)
 # API key, number of questions, model
 with st.expander('Groq API Key & Other settings'):
     groq_api_key = st.text_input(label='Enter your Groq API key here:')
-
-    # n_questions_col, model_name_col = st.columns([1, 3], vertical_alignment = 'center')
-
-    # with n_questions_col:
-    #     n_questions = st.number_input(label="Number of questions:",
-    #                                 min_value=5,
-    #                                 max_value=10,
-    #                                 value=5,
-    #                                 step=1)
-    # with model_name_col:
-    #     model_name = st.selectbox(label="Model:",
-    #                             options=["Llama 3.1 70B", "Llama 3.1 8B"],
-    #                             index=0)
+    if st.button('Save'):
+        st.session_state['groq_api_key'] = groq_api_key
 
 # Job description
 job_desc = st.text_area(label='Enter your job description here:')
 
 # generate questions
 if st.button('Generate questions'):
-    questions = utils.get_questions(job_desc=job_desc, n_questions=2, api_key=groq_api_key)
-    st.session_state['questions'] = questions
+    if 'groq_api_key' not in st.session_state:
+        st.error('Please enter your Groq API key first!')
+    else:
+        question_generation_status, questions = utils.get_questions(
+            job_desc=job_desc, 
+            n_questions=2, 
+            api_key=groq_api_key
+            )
+        if question_generation_status:
+            st.session_state['questions'] = questions
+            st.session_state['answers'] = {q: {sq: "" for sq in subq} for q, subq in questions.items()}
+            st.toast('Questions generated successfully!', icon='✅')
+        else:
+            st.error(questions)
+
+def save_answer(q_no, sub_q_no):
+    st.session_state.answers[q_no][sub_q_no] = st.session_state[f"answer_{q_no}_{sub_q_no}"]
 
 # display questions
 if 'questions' in st.session_state:
-    for q_no, question in enumerate(st.session_state['questions']):
-        with st.expander(f'Question {q_no+1}'):
-            st.write(question)
-        
+    for q_no in st.session_state['questions']:
+        with st.expander(f'Question {q_no}'):
+            for sub_q_no in st.session_state['questions'][q_no]:
+                st.write(st.session_state['questions'][q_no][sub_q_no])
+                st.text_area(
+                    "Answer", 
+                    key=f"answer_{q_no}_{sub_q_no}", 
+                    value=st.session_state.answers[q_no][sub_q_no]
+                    )
+                st.button(
+                    "Save answer", 
+                    key=f"save_{q_no}_{sub_q_no}", 
+                    on_click=save_answer, 
+                    args=(q_no, sub_q_no)
+                    )
+
+# Display all answers
+if st.button("Show all answers"):
+    for q_no, subquestions in st.session_state.questions.items():
+        st.write(f"Question {q_no}:")
+        for sub_q_no, question in subquestions.items():
+            st.write(f"  {sub_q_no}. {question}")
+            st.write(f"  Answer: {st.session_state.answers[q_no][sub_q_no]}")
+        st.write("---")
